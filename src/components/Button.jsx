2f6ca8f5cb5718a848c0eafdd7e8b2baffebdc1f
tobias-fletcher <tobias_fletcher@icloud.com>
2022-07-13 22:54:53 -0400

    Add storybook
    - add  storybook
    - start transfer of button component to stories folder

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,20 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { color } from 'styled-system'
+import { color } from 'styled-system';
 
 const Button = styled.button`
 
   box-sizing: border-box;
   display: inline-block;
   text-align: center;
+  height: 50%;
   border: 1px solid;
-  padding: 8px 16px;
+  padding: 9px 12px;
   background-color: yellow;
-  border-radius: 5px;
+  border-radius: 6px;
   margin: 1rem;
   font-weight: bold;
+  font-size: 12px;
   background: ${props => props.dark ? "#3e0254" : "#aaaaaa"};
   color: ${props => props.dark ? "#aaaaaa" : "#3e0254"};
 
@@ -21,15 +23,13 @@ const Button = styled.button`
   &:focus {
     outline: 0;
     color: white;
-    background-color: #9d00a2;
+    background-color: 
+    #302833
+    ;
     cursor: pointer;
   }
 
 `;
-
-
-
-
 
 export default Button;
 
@@ -42,3 +42,5 @@ Button.propTypes = {
   disabled: PropTypes.bool,
   children: PropTypes.node.isRequired,
 };
+
+export const Primary = () => <Button>Button</Button>;
--- src/pages/ButtonView.jsx
+++ src/pages/ButtonView.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import './ButtonView.css';
-
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { color } from 'styled-system';
 import Button from '../components/Button';
 
 
 function ButtonView() {
 
   return (
-    <>
+
+    <div className='buttonView'>
+      <h1>Button Variations</h1>
       <div>
-        <h1>Button Variations</h1>
-        <div>
-          <Button dark>Button</Button>
-          <Button>Button</Button>
-        </div>
+        <Button dark>BUTTON</Button>
+        <Button>BUTTON</Button>
       </div>
-    </>
+    </div>
+
   )
 }
 
 export default ButtonView;
+
+
+
